8373474756e6cfdb98fc4c5cd3edba3616f90adc
Azkalyasin <yasinadzkiaazkal@gmail.com>
2025-11-06 16:01:40 +0700

    melakukan loop terhadap jenis kemin dan ditampilkan dalam row

--- app/src/main/java/com/example/praktikum6/view/Formulir.kt
+++ app/src/main/java/com/example/praktikum6/view/Formulir.kt
@@ -6,8 +6,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.DividerDefaults.Thickness
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -21,6 +23,7 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.example.praktikum6.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormIsian (
     jenisK: List<String> = listOf ("laki-laki", "perempuan"),
@@ -53,7 +56,18 @@ fun FormIsian (
                 .padding(all = 20.dp)
                 .width(width = 250.dp), thickness = Thickness, color = Color.Red
             )
-
+            Row {
+                jenisK.forEach {
+                    item->
+                    Row(verticalAlignment = Alignment.CenterVertically){
+                        RadioButton(
+                            selected = false,
+                            onClick = {}
+                        )
+                        Text(text = item)
+                    }
+                }
+            }
         }
     }
 }
